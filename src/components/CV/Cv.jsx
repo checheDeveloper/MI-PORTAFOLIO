9b71e7697d891505a2d82509f4c4c55be00151c7
Adriana <mafo2767@gmail.com>
2021-11-12 18:17:30 -0500

    component cv made

---
+++ src/components/CV/Cv.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Cv () {
+    return (
+      <div>
+        <header>
+         <p>WHAT JAPEN BRO?</p>
+        </header>
+      </div>
+    );
+  }
+  
+  export default Cv;
